d37a1c20e6e49f1938601886aa8842324120c22a
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2022-05-03 09:15:06 +0200

    Rework before changements

--- Patch POO/Paddle.py
+++ Patch POO/Paddle.py
@@ -1,14 +1,21 @@
 import turtle
+import Playground
 
 
 class Paddle:
-    def __init__(self, speed=int, shape=str, color=str, xcor=int):
-        speed = self.speed
-        shape = self.shape
-        color = self.color
-        xcor = self.xcor
+    def __init__(self, speed, shape, color, xcor):
+        self.speed = speed
+        self.shape = shape
+        self.color = color
+        self.xcor = xcor
+
+    def initialisation(self, p):
+        p.shapesize(stretch_wid=5, stretch_len=1)
+        p.penup()
+        p.goto(paddle.xcor, 0)
 
     def moving_up(self, key_pressed):
+        screen = Playground.creating_playground()
         if screen.onkeypress(key_pressed):
             y = Paddle.ycor()
             y += 20
@@ -16,24 +23,20 @@ class Paddle:
 
 
     def moving_down(self, key_pressed):
+        screen = Playground.creating_playground()
         if screen.onkeypress(key_pressed):
             y = Paddle.ycor()
             y -= 20
             Paddle.sety(y)
 
-    def initialisation(self, paddle):
-        Paddle = turtle.Turtle()
-        Paddle.shapesize(stretch_wid=5, stretch_len=1)
-        Paddle.penup()
-        Paddle.goto(paddle.xcor, 0)
 
-
-def creating_paddles():
-    left_paddle = Paddle(speed=0, shape="square", color="white", xcor=-350)
-    right_paddle = Paddle(speed=0, shape="square", color="white", xcor=350)
-    Paddle.initialisation(left_paddle)
-    Paddle.initialisation(right_paddle)
-    left_paddle.moving_down(key_pressed="s")
-    left_paddle.moving_up(key_pressed="z")
-    right_paddle.moving_down(key_pressed="Down")
-    right_paddle.moving_up(key_pressed="Up")
+left_paddle = Paddle(speed=0, shape="square", color="white", xcor=-350)
+right_paddle = Paddle(speed=0, shape="square", color="white", xcor=350)
+left_paddle = turtle.Turtle()
+right_paddle = turtle.Turtle()
+Paddle.initialisation(left_paddle)
+Paddle.initialisation(right_paddle)
+left_paddle.moving_down(key_pressed="s")
+left_paddle.moving_up(key_pressed="z")
+right_paddle.moving_down(key_pressed="Down")
+right_paddle.moving_up(key_pressed="Up")
