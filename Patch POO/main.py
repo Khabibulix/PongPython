aff67c0217e5846a91ae31e527b35d3d214e78ab
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2022-05-03 08:53:39 +0200

    Create main.py

---
+++ Patch POO/main.py
@@ -0,0 +1,17 @@
+# Import and Setup
+import turtle
+import winsound
+import Playground
+import Ball
+import Paddle
+
+
+# Preparing game
+Playground.creating_playground()
+Paddle.creating_paddles()
+Ball.creating_ball()
+
+#Main game
+while True:
+    turtle.update()
+    Ball.ball.collision_detection()
