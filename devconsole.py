d8c33437ac5442501f6d1feb8758eb14d3449491
Pitchumi <NoMailYet>
2022-05-20 14:00:55 +0200

    init devconsole.py A simple init with organisation in comment to prepare the work

---
+++ devconsole.py
@@ -0,0 +1,43 @@
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+#   DevConsole for cheats and testing
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+#   Possible commands TODO :
+#   ### Dev
+#   # - Enable/Disable game event log print     #-// {Bool}
+#   ### Cheats
+#   # - Add ball                                #-// No param
+#   # - Remove ball                             #-// No param
+#   # - Reset game                              #-// No param
+#   # - Enable/Disable Ball raytrace            #-// {Bool}
+#   # - Change pad size                         #-// {Pad id} {Size}
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+#   Functionnalities TODO :
+#   ### Isolated part
+#   # - [X] devconsole.py existence
+#   # - [ ] Read input
+#   # - [ ] Treat the input and print result
+#   # - [ ] User command log
+#   ### Combined with PongPython game
+#   # - [ ] In-game window
+#   # - [ ] Window appearance based on a button press (maybe TAB key)
+#   # - [ ] Typing box
+#   # - [ ] Game event log
+#   # - [ ] Scrollable log
+#____________________________________#
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+# CODE BEGIN
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+# IMPORT
+#
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+# MACROS
+#
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+# GLOBAL
+#
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+# FUNCTIONS
+#
+##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
+# MAIN
+#
