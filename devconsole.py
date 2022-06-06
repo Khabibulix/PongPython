8a6058facb37b978a96039219e4ddac8d1a9b658
Pitchumi <notyet>
2022-06-06 22:02:49 +0200

    Input loop

--- devconsole.py
+++ devconsole.py
@@ -38,22 +38,47 @@ import sys
 #
 argv = sys.argv
 argcount = len(argv)
-argcommand = argv[1]
+if argcount > 1:
+    argcommand = argv[1]
 commandlist = ["dev_gamelog","game_ball_add","game_ball_remove","game_reset","game_ball_retrace","game_pad_size"]
+log = []
 
 ##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
 # FUNCTIONS
 #
 def print_undefined(arg):
-    print("Error : command \"" + arg + "\" does not exist.")
+    message = "Error : command \"" + arg + "\" does not exist."
+    log.append(message)
+    print(message)
 
 def print_wip(arg):
-    print("Sorry : command \"" + arg + "\" is not implemented yet.")
+    message = "Sorry : command \"" + arg + "\" is not implemented yet."
+    log.append(message)
+    print(message)
 
 def print_help():
     print("Available commands :")
     for i in commandlist :
+        log.append(i)
         print(i)
+
+def console_input(arg):
+    if arg == "help":
+        print_help()
+    elif arg == "dev_gamelog":
+        print_wip(arg)
+    elif arg == "game_ball_add":
+        print_wip(arg)
+    elif arg == "game_ball_remove":
+        print_wip(arg)
+    elif arg == "game_reset":
+        print_wip(arg)
+    elif arg == "game_ball_retrace":
+        print_wip(arg)
+    elif arg == "game_pad_size":
+        print_wip(arg)
+    else:
+        print_undefined(arg)
 
 ##~~~~~~~~~~~~~~~~-~~~~~~~~~~~~~~~~~##
 # MAIN
@@ -65,22 +90,8 @@ print("Command : " + argcommand)
 """
 
 if argcount > 1:
-    if argcommand == "" or argcommand == "help":
-        print_help()
-    elif argcommand == "dev_gamelog":
-        print_wip(argcommand)
-    elif argcommand == "game_ball_add":
-        print_wip(argcommand)
-    elif argcommand == "game_ball_remove":
-        print_wip(argcommand)
-    elif argcommand == "game_reset":
-        print_wip(argcommand)
-    elif argcommand == "game_ball_retrace":
-        print_wip(argcommand)
-    elif argcommand == "game_pad_size":
-        print_wip(argcommand)
-    else:
-        print_undefined(argcommand)
-    
-else :
-    pass
+    console_input(argcommand)
+while(1):
+    usercommand = input("- ")
+    log.append(usercommand)
+    console_input(usercommand)
