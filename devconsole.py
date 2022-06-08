7543d2aae0c54a04eb54371bcca5ca807d3da68d
Pitchumi <notyet>
2022-06-08 10:19:29 +0200

    fixup! Help command with command name, parameters and use

--- devconsole.py
+++ devconsole.py
@@ -14,9 +14,9 @@
 #   Functionnalities TODO :
 #   ### Isolated part
 #   # - [X] devconsole.py existence
-#   # - [ ] Read input
-#   # - [ ] Treat the input and print result
-#   # - [ ] User command log
+#   # - [X] Read input
+#   # - [X] Treat the input and print result
+#   # - [X] User command log
 #   ### Combined with PongPython game
 #   # - [ ] In-game window
 #   # - [ ] Window appearance based on a button press (maybe TAB key)
